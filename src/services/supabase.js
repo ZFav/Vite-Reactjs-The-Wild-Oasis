b74e367681960d47a8a9781bdcdb11a477eed9ad
Fabio Zhang <Flasvert@gmail.com>
2023-09-08 15:18:02 +0200

    termine supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://jugxgiluqccqbnzrporr.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1Z3hnaWx1cWNjcWJuenJwb3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQxNzUyNzMsImV4cCI6MjAwOTc1MTI3M30.jyv1LMGM-DJTd6H12QA-QhGdpucUjg2XAyhzi-Zkmbw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
